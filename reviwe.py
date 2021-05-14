9afe291901e530f69f840bfdd93f0aa330b164a9
yaumaorange <82813858+yaumaorange@users.noreply.github.com>
2021-05-14 10:27:26 +0800

    add word count

--- reviwe.py
+++ reviwe.py
@@ -14,8 +14,41 @@ for d in data:
 avg_len = sum_len / len(data)
 print('每筆留言平均長度為', avg_len)
 
-new = []
-for d in data:
-	if len(d) < 100:
-		new.append(d)
+new = [d for d in data if len(d) > 100]
 print('字數小於100的資料共有', len(new), '筆')
+
+good = [d for d in data if 'good' in d]
+print('好評筆數共有', len(good))
+
+#文字計數
+wc = {} #word_count
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 #新增key
+
+for word in wc:
+	if wc[word] > 100000:
+		print(word, wc[word])
+
+print(len(wc))
+
+while True:
+	word = input('請輸入關鍵字:')
+	if word = 'q':
+		break
+	if word in wc:
+		print(word, '的出現次數為:', wc[word])
+	else:
+		print('沒有這個字')
+
+
+print('感謝查詢')
+
+
+
+
+
